bb5633f0aab4a3ce57233080308e7f93aa5548a4
officialconfuzius <conrad.niewienda@hotmail.de>
2023-05-31 02:01:20 +0200

    #1 test

--- setuppalmtree/how2use.py
+++ setuppalmtree/how2use.py
@@ -7,7 +7,7 @@ import torch
 import numpy as np
 import eval_utils as utils
 
-
+#thisisatest
 palmtree = utils.UsableTransformer(model_path="./palmtree/transformer.ep19", vocab_path="./palmtree/vocab")
 files=[]
 for f in os.listdir():
